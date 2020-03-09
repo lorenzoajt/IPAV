3fedd75002cfaba90e69a3057f4e2f1cb6bbc563
dstprojects <36209165+dstprojects@users.noreply.github.com>
2020-03-09 16:23:45 -0600

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -71,10 +71,19 @@ function mousePressed() {
 
 // A simple Particle class
 let Particle = function(position) {
-  this.acceleration = createVector(0, 0.05);
-  this.velocity = createVector(random(-1, 1), random(-1, 0));
+  // this.acceleration = createVector(0, 0);
+  var velx = 0;
+  var vely = 0;
+
+  do{
+    velx = random(-1,1);
+    vely = random(-1,1);
+  }while(velx == 0 || vely == 0);
+
+  this.velocity = createVector(velx, vely);
   this.position = position.copy();
-  this.lifespan = 255.0;
+  // this.lifespan = 255.0;
+  
 };
 
 Particle.prototype.run = function() {
@@ -86,21 +95,23 @@ Particle.prototype.run = function() {
 Particle.prototype.update = function(){
   this.velocity.add(this.acceleration);
   this.position.add(this.velocity);
-  this.lifespan -= 2;
+  // this.lifespan -= 2;
+
 };
 
 // Method to display
 Particle.prototype.display = function () {
-  stroke(200, this.lifespan);
-  strokeWeight(2);
-  fill(127, this.lifespan);
+  stroke(0,50,103);
+  strokeWeight(1);
+  fill(18,164,241,125);
   ellipse(this.position.x, this.position.y, 12, 12);
 };
 
 // Is the particle still useful?
 Particle.prototype.isDead = function () {
-  if (this.lifespan < 0) {
+  if (this.position.x <= 0 || this.position.x >= windowWidth || this.position.y <= 0 || this.position.y >= windowHeight) {
     return true;
+
   } else {
     return false;
   }
@@ -171,7 +182,7 @@ CrazyParticle.prototype.display=function() {
   push();
   translate(this.position.x, this.position.y);
   rotate(this.theta);
-  stroke(255, this.lifespan);
+  stroke(124,143,172);
   line(0, 0, 25, 0);
   pop();
 }
